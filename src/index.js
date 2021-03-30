3231868adec70c9592142df8fbd1df1d1d8d9a57
Luzciel <luzcielmontesinos@gmail.com>
2021-03-30 14:07:02 -0300

    integracion del la portada  y el portafolio. Resposive para celular TODO: responsive table

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// // setTimeout(bottomConoceme,4000);
+const conoceme = document.getElementById('button-conoceme')
+
+console.log(11, conoceme)
+const bottomConoceme = () => {
+    console.log(2222)
+    conoceme.style.visibility = 'visible';
+}
+
+setTimeout(bottomConoceme, 2500);
+
